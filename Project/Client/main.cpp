94d8e09831ebabe9a6225e83c1e61c08c2aeed32
DevCyoung <tkdlqldjq123@gmail.com>
2023-05-18 01:26:39 +0900

    feat: .gitignore
    */bin_d/*.exe -> **/bin_d/*.exe (change 4)

--- Project/Client/main.cpp
+++ Project/Client/main.cpp
@@ -37,7 +37,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
-       
+    
     
     
     // TODO: 여기에 코드를 입력합니다.
