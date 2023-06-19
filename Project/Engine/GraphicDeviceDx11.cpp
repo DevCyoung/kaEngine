55a27b03ce7570e0307101ea5ee958e7aad11fd1
DevCyoung <tkdlqldjq123@gmail.com>
2023-06-20 08:15:18 +0900

    feat: mesh fix

--- Project/Engine/GraphicDeviceDx11.cpp
+++ Project/Engine/GraphicDeviceDx11.cpp
@@ -333,6 +333,50 @@ namespace engine::graphics
 	{
 #pragma region Creatge Meshs
 		//RectMesh
+		//RectMesh
+		{
+			const UINT VERTEX_COUNT = 6;
+
+			D3D11_BUFFER_DESC triangleDesc = {};
+			triangleDesc.Usage = D3D11_USAGE::D3D11_USAGE_DYNAMIC;
+			triangleDesc.ByteWidth = sizeof(tVertex) * 6;
+			triangleDesc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_VERTEX_BUFFER;
+			triangleDesc.CPUAccessFlags = D3D11_CPU_ACCESS_FLAG::D3D11_CPU_ACCESS_WRITE;
+
+			D3D11_SUBRESOURCE_DATA triangleData = {};
+			tVertex triVertexes[VERTEX_COUNT] = {};
+
+			triVertexes[0].pos = Vector3(-0.5f, 0.5f, 0.0f);
+			triVertexes[0].color = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
+			triVertexes[0].uv = Vector2(0.0f, 0.0f);
+
+			triVertexes[1].pos = Vector3(0.5f, -0.5f, 0.0f);
+			triVertexes[1].color = Vector4(0.0f, 1.0f, 0.0f, 1.0f);
+			triVertexes[1].uv = Vector2(1.0f, 1.0f);
+
+			triVertexes[2].pos = Vector3(-0.5f, -0.5f, 0.0f);
+			triVertexes[2].color = Vector4(0.0f, 0.0f, 1.0f, 1.0f);
+			triVertexes[2].uv = Vector2(0.0f, 1.0f);
+
+			triVertexes[3].pos = Vector3(-0.5f, 0.5f, 0.0f);
+			triVertexes[3].color = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
+			triVertexes[3].uv = Vector2(0.0f, 0.0f);
+
+			triVertexes[4].pos = Vector3(0.5f, 0.5f, 0.0f);
+			triVertexes[4].color = Vector4(0.0f, 0.0f, 1.0f, 1.0f);
+			triVertexes[4].uv = Vector2(1.0f, 0.0f);
+
+			triVertexes[5].pos = Vector3(0.5f, -0.5f, 0.0f);
+			triVertexes[5].color = Vector4(0.0f, 1.0f, 0.0f, 1.0f);
+			triVertexes[5].uv = Vector2(1.0f, 1.0f);
+
+			triangleData.pSysMem = triVertexes;
+
+			gResourceManager->Insert<Mesh>(new Mesh(triangleDesc, &triangleData,
+				sizeof(tVertex), VERTEX_COUNT, mDevice.Get()), L"Rect");
+		}
+
+
 
 #pragma endregion
 
