a5969b9143c4f46e7287069bd9c01bb709112e52
DevCyoung <tkdlqldjq123@gmail.com>
2023-06-23 05:49:24 +0900

    feat: Helper: EnumResource Message show program, ex) EnumResource: blabla~

--- Helper/ConsoleApp1/ConsoleApp1/Main.cs
+++ Helper/ConsoleApp1/ConsoleApp1/Main.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.Versioning;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
@@ -12,24 +14,45 @@ using System.Text;
 
 public class MyProgram
 {
-    static string SHARP_INCLUDE = "#include";
-    //static string SHARP_DEFINE= "#define";
+    static string SHARP_INCLUDE = "#include";    
     static string BEGIN_ENGINE_NAMESPACE = "namespace engine\n{\n";
     static string END_ENGINE_NAMESPACE = "}\n";
     static string RESOURCE_PATH = "";
 
-    public static void Main(string[] args)
+    static void WriteLine(string str)
     {
-        //string path = @"C:\Users\tkdlq\Desktop\assort\DirectX2D_Engine\Output\bin\resource";
-        //string targetPath = @"C:\Users\tkdlq\Desktop\assort\DirectX2D_Engine\Output\bin";
+        Console.WriteLine($"EnumProgram: {str}");
+    }
 
-        
+    static void WriteWarningLine(string str)
+    {
+        Console.WriteLine($"EnumProgram: Warning: {str}");
+    }
 
-        string path = @"";
-        string targetPath = @"";
+    static void WriteErrorLine(string str)
+    {
+        Console.WriteLine($"EnumProgram: Error: {str}");
+    }
+
+    public static void Main(string[] args)
+    {                    
+        WriteLine("Program Ready.....");
+        WriteLine("Input Check....");
+        WriteLine("###########Input Path############");
+        WriteLine($"argc = {args.Length}");
+        foreach (string arg in args)
+        {            
+            WriteLine($"arg = {arg}");
+        }
+        WriteLine("###########Input Path############");
 
         if (args.Length == 2)
-        {            
+        {
+            WriteLine("args length is 2, ok! Program start");
+
+            string path = @"";
+            string targetPath = @"";
+
             path += args[0];
             targetPath += args[1];
 
@@ -43,18 +66,18 @@ public class MyProgram
             string resourcePath = path;
 
             string[] resourceDirectorys = Directory.GetDirectories(resourcePath);
+
             if (null == resourceDirectorys)
             {
-                Console.WriteLine("resourceDirectorys is null");
+                WriteErrorLine($"resourceDirectorys is null has directory? path({resourcePath})");
                 return;
             }
-
             
-
+            WriteLine("###########Create Files############");
             foreach (string resDirectory in resourceDirectorys)
             {
                 string enumType = GetFileName(resDirectory);
-                
+
 
                 List<string> enumNames = new List<string>();
                 List<string> enumFilePath = new List<string>();
@@ -64,14 +87,23 @@ public class MyProgram
                 string enumHeader = MakeEnumNumHeader(enumType, enumNames);
 
                 string enumHeaderPath =
-                   targetPath + "EnumResourceType" + enumType + ".h";               
+                   targetPath + "EnumResourceType" + enumType + ".h";
 
                 StreamWriter enumHearderWriter = new StreamWriter(
                 new FileStream(enumHeaderPath
                 , FileMode.Create)
                 , Encoding.UTF8);
+                if (null == enumHearderWriter)
+                {
+                    WriteErrorLine($"enumHearderWriter, check your enumHeaderPath... enumHeaderPath(Create fullPath) = " +
+                        $"{enumHeaderPath} targetPath = {targetPath}");
+                    return;
+                }
+
                 enumHearderWriter.Write(enumHeader);
+                WriteLine($"CreateHeaderFile = { enumHeaderPath}");
                 enumHearderWriter.Close();
+
 
 
 
@@ -84,25 +116,27 @@ public class MyProgram
                 new FileStream(enumCppPath
                 , FileMode.Create)
                 , Encoding.UTF8);
+
+                if (null == enumCppWirter)
+                {
+                    WriteErrorLine($"enumCppWirter, check your enumCppPath... enumCppPath(Create fullPath) = " +
+                        $"{enumCppPath} targetPath = {targetPath}");
+                    return;
+                }
+
+
                 enumCppWirter.Write(enumCpp);
-                enumCppWirter.Close();
-
-
-
-
-
-
-
-
+                enumCppWirter.Close();                
+                WriteLine($"CreateCppFile = {enumCppPath}");
             }
-
-            //EnumResourceEna
+            WriteLine("###########Create Files############");
+            WriteLine("Success End...");            
         }
         else
-        {
-            Console.WriteLine("need 2 arguments");
+        {            
+            WriteWarningLine("need 2 arguments, No Execute Enum Program End");            
         }
-        
+                        
     }
 
     public static string[] GetDirectories(string resourcePath)
@@ -127,9 +161,9 @@ public class MyProgram
         return fileName;
     }
 
-    public static void MakeEnumNames(string resDirectory, 
+    public static void MakeEnumNames(string resDirectory,
         List<string> enumNames,
-        List<string> enumFilePath, 
+        List<string> enumFilePath,
         string recursiveEnumName)
     {
         string[] directorys = Directory.GetDirectories(resDirectory);
@@ -145,23 +179,23 @@ public class MyProgram
         }
 
         string[] fileNames = Directory.GetFiles(resDirectory);
-        
 
-        
+
+
 
         foreach (string fileName in fileNames)
         {
             string perfactEnumName = recursiveEnumName + Path.GetFileNameWithoutExtension(
                 GetFileName(fileName));
             enumNames.Add(perfactEnumName);
-            
+
             string resourceBaseFileName = fileName.Replace(resDirectory, "");
             resourceBaseFileName = resourceBaseFileName.Replace("\\", "\\\\");
             enumFilePath.Add(fileName.Replace(RESOURCE_PATH, "").Replace("\\", "\\\\"));
 
         }
     }
-    
+
 
     public static string MakeEnumNumHeader(string enumType, List<string> enumNames)
     {
@@ -181,12 +215,12 @@ public class MyProgram
         }
         header += "\t" + "\tEnd\n";
         header += "\t};\n\n";
-        
+
         if (enumNames.Count > 0)
         {
             header += $"\tconst wchar_t* EnumResourcePath(eRes{enumType} type);\n";
-        }        
-        
+        }
+
         header += "}\n";
 
         return header;
@@ -198,7 +232,7 @@ public class MyProgram
         string cpp = "";
         string eResEnumType = $"eRes{enumType}";
         string enumResourcePathArray = $"{eResEnumType}Path";
-        
+
 
         cpp += $"{SHARP_INCLUDE} \"pch.h\"\n";
         cpp += $"{SHARP_INCLUDE} \"EnumResourceType{enumType}.h\"\n";
@@ -208,9 +242,10 @@ public class MyProgram
 
         //const wchar
         if (relativePaths.Count > 0)
-        {
+        {            
             cpp += $"\tconst wchar_t* {enumResourcePathArray}[static_cast<UINT>({eResEnumType}::End)]\n";
             cpp += "\t{\n";
+
             foreach (string relativePath in relativePaths)
             {
                 //리소스 하위로가져온다.
@@ -223,36 +258,11 @@ public class MyProgram
             cpp += $"\t\treturn {enumResourcePathArray}[static_cast<UINT>(type)];\n";
             cpp += "\t}\n";
         }
-        
-        cpp += END_ENGINE_NAMESPACE;
 
-        //const char* EnumResourceToString(COMPONENT_TYPE _type) { return COMPONENT_TYPE_LPCSTR[(UINT)_type]; }
-        //const wchar_t* EnumResourceToWString(COMPONENT_TYPE _type) { return COMPONENT_TYPE_LPCWSTR[(UINT)_type]; }
-
+        cpp += END_ENGINE_NAMESPACE; 
         return cpp;
     }
 
 
-    
+
 }
-////COMPONENT_TYPE
-//extern const char* COMPONENT_TYPE_LPCSTR[(UINT)COMPONENT_TYPE::SCRIPT + 1]
-//{
-//	"TRANSFORM",
-//	"COLLIDER2D",
-//	"COLLIDER3D",
-//	"RIGIDBODY2D",
-//	"ANIMATOR2D",
-//	"ANIMATOR3D",
-//	"LIGHT2D",
-//	"LIGHT3D",
-//	"CAMERA",
-//	"MESHRENDER",
-//	"PARTICLESYSTEM",
-//	"TILEMAP",
-//	"LANDSCAPE",
-//	"DECAL",
-//	"LINECOLLIDER2DS",
-//	"END",
-//	"SCRIPT",
-//};
