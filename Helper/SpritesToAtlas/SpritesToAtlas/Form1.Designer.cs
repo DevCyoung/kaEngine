e62cd11c2934d8c73fd78fbf78d9c5e1667b07f8
DevCyoung <tkdlqldjq123@gmail.com>
2023-07-22 08:50:17 +0900

    feat: SpritesToAtlas simple test OK

--- Helper/SpritesToAtlas/SpritesToAtlas/Form1.Designer.cs
+++ Helper/SpritesToAtlas/SpritesToAtlas/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SelectSprites = new Button();
+            AtlasView = new PictureBox();
+            AtlasPanel = new Panel();
+            ButtonExport = new Button();
+            ((System.ComponentModel.ISupportInitialize)AtlasView).BeginInit();
+            AtlasPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // SelectSprites
+            // 
+            SelectSprites.BackColor = SystemColors.ActiveCaption;
+            SelectSprites.Location = new Point(731, 12);
+            SelectSprites.Name = "SelectSprites";
+            SelectSprites.Size = new Size(104, 42);
+            SelectSprites.TabIndex = 0;
+            SelectSprites.Text = "SelectSprites";
+            SelectSprites.UseVisualStyleBackColor = false;
+            SelectSprites.Click += button1_Click;
+            // 
+            // AtlasView
+            // 
+            AtlasView.BackColor = SystemColors.AppWorkspace;
+            AtlasView.Cursor = Cursors.Cross;
+            AtlasView.Location = new Point(7, 6);
+            AtlasView.Name = "AtlasView";
+            AtlasView.Size = new Size(700, 400);
+            AtlasView.TabIndex = 1;
+            AtlasView.TabStop = false;
+            // 
+            // AtlasPanel
+            // 
+            AtlasPanel.AutoScroll = true;
+            AtlasPanel.BackColor = SystemColors.ActiveCaption;
+            AtlasPanel.Controls.Add(AtlasView);
+            AtlasPanel.Location = new Point(12, 12);
+            AtlasPanel.Name = "AtlasPanel";
+            AtlasPanel.Size = new Size(713, 790);
+            AtlasPanel.TabIndex = 2;
+            // 
+            // ButtonExport
+            // 
+            ButtonExport.Location = new Point(731, 60);
+            ButtonExport.Name = "ButtonExport";
+            ButtonExport.Size = new Size(104, 41);
+            ButtonExport.TabIndex = 3;
+            ButtonExport.Text = "Export";
+            ButtonExport.UseVisualStyleBackColor = true;
+            ButtonExport.Click += ButtonExport_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(839, 814);
+            Controls.Add(ButtonExport);
+            Controls.Add(AtlasPanel);
+            Controls.Add(SelectSprites);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)AtlasView).EndInit();
+            AtlasPanel.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button SelectSprites;
+        private PictureBox AtlasView;
+        private Panel AtlasPanel;
+        private Button ButtonExport;
     }
 }
--- Helper/SpritesToAtlas/SpritesToAtlas/Form1.cs
+++ Helper/SpritesToAtlas/SpritesToAtlas/Form1.cs
@@ -1,10 +1,153 @@
+using System.Drawing.Imaging;
+
 namespace SpritesToAtlas
 {
     public partial class Form1 : Form
     {
+        public Point cursurPos;
+        public Size pad;
+        public Size leftTopMargin;
+
+        public Font font;
+        public Bitmap atlasImage;
+
+        OpenFileDialog odf;
+        SaveFileDialog sdf;
+
         public Form1()
         {
             InitializeComponent();
+
+            AtlasView.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            leftTopMargin = new Size(5, 5);
+            cursurPos = new Point(leftTopMargin.Width, leftTopMargin.Height);
+            pad = new Size(10, 20);
+            atlasImage = new Bitmap(AtlasView.Width, AtlasView.Height);
+
+            font = new Font("Arial", 10);
+
+            odf = new OpenFileDialog();
+            odf.Filter = "Image Files (*.png, *.jpg, *.bmp)|*.png;*.jpg;*.bmp";
+            odf.Multiselect = true;
+
+            sdf = new SaveFileDialog();
+            sdf.Filter = "Image Files (*.png, *.jpg, *.bmp)|*.png;*.jpg;*.bmp";
+        }
+
+        private void lineFead()
+        {
+            cursurPos.Y += pad.Height;
+        }
+
+        private void carriageReturn()
+        {
+            cursurPos.X = leftTopMargin.Width;
+        }
+
+        private void moveCursurX(int width)
+        {
+            cursurPos.X += width;
+        }
+        private void moveCursurY(int height)
+        {
+            cursurPos.Y += height;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics atalsGraphics = Graphics.FromImage(atlasImage);
+
+            if (DialogResult.OK == odf.ShowDialog())
+            {
+                string[] files = odf.FileNames;
+
+                if (files.Length == 0)
+                {
+                    return;
+                }
+
+                //Load Image
+                Bitmap[] sprties = new Bitmap[files.Length];
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    sprties[i] = new Bitmap(files[i]);
+                }
+
+                int maxSpriteHeight = 0;
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    if (sprties[i].Height > maxSpriteHeight)
+                    {
+                        maxSpriteHeight = sprties[i].Height;
+                    }
+                }
+
+
+
+                atalsGraphics.DrawString(odf.SafeFileNames[0],
+                    font, Brushes.Yellow, cursurPos.X, cursurPos.Y);
+
+                moveCursurY(font.Height);
+                carriageReturn();
+                lineFead();
+
+
+
+                //Draw Bmp
+                for (int i = 0; i < files.Length; i++)
+                {
+
+                    if (cursurPos.X + sprties[i].Width >= atlasImage.Width + pad.Width)
+                    {
+                        carriageReturn();
+                        moveCursurY(maxSpriteHeight);
+                        lineFead();
+                    }
+
+                    if (atlasImage.Height < cursurPos.Y + pad.Height + maxSpriteHeight)
+                    {
+                        Bitmap newBitmp = new Bitmap(atlasImage.Width, atlasImage.Height + 400);
+                        Graphics newGraphics = Graphics.FromImage(newBitmp);
+
+                        newGraphics.DrawImage(atlasImage, 0, 0);
+
+                        atlasImage = newBitmp;
+                        atalsGraphics = newGraphics;
+                    }
+
+
+                    atalsGraphics.DrawImage(sprties[i], cursurPos.X, cursurPos.Y);
+
+
+                    //Update x,y
+                    moveCursurX(sprties[i].Width + pad.Width);
+                }
+
+                carriageReturn();
+                moveCursurY(maxSpriteHeight);
+                lineFead();
+
+                AtlasView.Image = atlasImage;
+
+            }
+        }
+
+        private void ButtonExport_Click(object sender, EventArgs e)
+        {
+            sdf.ShowDialog();
+
+            if (sdf.FileName != "")
+            {
+                atlasImage.Save(sdf.FileName, ImageFormat.Png);
+                //atlasImage.Save(sdf.FileName, ImageFormat.Png);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a file name.");
+            }
         }
     }
 }
